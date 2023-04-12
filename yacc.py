88fa5dea38f3fb1cca31b75bea5922b9b4d9c8eb
gabriel gomez <a01720009@itesm.mx>
2023-04-12 00:37:52 -0600

    se cambia numero de espacios en yacc

--- yacc.py
+++ yacc.py
@@ -5,7 +5,7 @@ from lex import literals
 # Empieza el programa
 def p_programa_start(p):
 	'''
-	programa_start		: PROGRAM ID ';' programa_start_1 BLOQUE
+	programa_start	: PROGRAM ID ';' programa_start_1 BLOQUE
 	'''
 def p_programa_start_1(p):
 	'''
@@ -15,122 +15,122 @@ def p_programa_start_1(p):
 	
 def p_VARS(p):
 	'''
-	VARS		        : VAR ID VARS_1 ':' TIPO ';' VARS_2 
+	VARS	: VAR ID VARS_1 ':' TIPO ';' VARS_2 
 	'''
 def p_VARS_1(p):
 	'''
-	VARS_1		        : ',' ID VARS_1 
-                        | empty
+	VARS_1	: ',' ID VARS_1 
+        | empty
 	'''
 def p_VARS_2(p):
 	'''
-	VARS_2		        : ID VARS_1 ':' TIPO ';' VARS_2
-                        | empty
+	VARS_2	: ID VARS_1 ':' TIPO ';' VARS_2
+        | empty
 	'''
 	
 def p_TIPO(p):
 	'''
-	TIPO		        : FLOAT
-                        | INT
+	TIPO	: FLOAT
+    	| INT
 	'''
 
 def p_BLOQUE(p):
 	'''
-	BLOQUE		        : '{' BLOQUE_1 '}'
+	BLOQUE	: '{' BLOQUE_1 '}'
 	'''
 def p_BLOQUE_1(p):
 	'''
-	BLOQUE_1		    : ESTATUTO BLOQUE_1
-                        | empty
+	BLOQUE_1	: ESTATUTO BLOQUE_1
+        | empty
 	'''
 
 def p_ESTATUTO(p):
 	'''
-	ESTATUTO		    : ASIGNACION
-                        | CONDICION
-			            | ESCRITURA
+	ESTATUTO	: ASIGNACION
+        | CONDICION
+		| ESCRITURA
 	'''
 
 def p_ASIGNACION(p):
 	'''
-	ASIGNACION		    : ID '=' EXPRESION ';'
+	ASIGNACION	: ID '=' EXPRESION ';'
 	'''	
 
 def p_EXPRESION(p):
 	'''
-	EXPRESION		    : EXP EXPRESION_1
+	EXPRESION	: EXP EXPRESION_1
 	'''		
 def p_EXP(p):
 	'''
-	EXP     		    : TERMINO EXP_1
+	EXP	: TERMINO EXP_1
 	'''	
 def p_EXP_1(p):
 	'''
-	EXP_1     		    : '+' EXP
-                        | '-' EXP
-                        | empty
+	EXP_1	: '+' EXP
+        | '-' EXP
+        | empty
 	'''	
 def p_EXPRESION_1(p):
 	'''
-	EXPRESION_1		    : '>' EXP
-                        | '<' EXP
-                        | NE EXP
-                        | empty
+	EXPRESION_1	: '>' EXP
+        | '<' EXP
+        | NE EXP
+        | empty
 	'''	
 	
 def p_ESCRITURA(p):
 	'''
-	ESCRITURA		    : PRINT '(' ESCRITURA_1 ')' ';'
+	ESCRITURA	: PRINT '(' ESCRITURA_1 ')' ';'
 	'''	
 def p_ESCRITURA_1(p):
 	'''
-	ESCRITURA_1		    : CTESTRING ESCRITURA_2
-                        | EXPRESION ESCRITURA_2
+	ESCRITURA_1	: CTESTRING ESCRITURA_2
+        | EXPRESION ESCRITURA_2
 	'''	
 def p_ESCRITURA_2(p):
 	'''
-	ESCRITURA_2		    : ',' ESCRITURA_1
-                        | empty
+	ESCRITURA_2	: ',' ESCRITURA_1
+    	| empty
 	'''	
 	
 def p_CONDICION(p):
 	'''
-	CONDICION		    : IF '(' EXPRESION ')' BLOQUE CONDICION_1 ';'
+	CONDICION	: IF '(' EXPRESION ')' BLOQUE CONDICION_1 ';'
 	'''	
 def p_CONDICION_1(p):
 	'''
-	CONDICION_1		    : ELSE BLOQUE
-                        | empty
+	CONDICION_1	: ELSE BLOQUE
+    	| empty
 	'''	
 
 def p_TERMINO(p):
 	'''
-	TERMINO 		    : FACTOR TERMINO_1
+	TERMINO	: FACTOR TERMINO_1
 	'''	
 def p_TERMINO_1(p):
 	'''
-	TERMINO_1 		    : '*' TERMINO
-                        | '/' TERMINO
-                        | empty
+	TERMINO_1	: '*' TERMINO
+    	| '/' TERMINO
+    	| empty
 	'''	
 
 def p_FACTOR(p):
 	'''
-	FACTOR   		    : '(' EXPRESION ')'
-                        | FACTOR_1 VAR_CTE
+	FACTOR	: '(' EXPRESION ')'
+		| FACTOR_1 VAR_CTE
 	'''	
 def p_FACTOR_1(p):
 	'''
-	FACTOR_1  		    : '+'
-                        | '-'
-                        | empty
+	FACTOR_1	: '+'
+    	| '-'
+    	| empty
 	'''	
 
 def p_VAR_CTE(p):
 	'''
-	VAR_CTE 		    : ID
-                        | FLOAT
-                        | INT
+	VAR_CTE	: ID
+    	| FLOAT
+    	| INT
 	'''	
 
 # Empty symbol = ε
